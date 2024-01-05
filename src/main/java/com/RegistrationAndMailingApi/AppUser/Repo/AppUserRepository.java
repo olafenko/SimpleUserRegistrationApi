1ccf2eb147a9529b9e4a8f0b58d56fd76815b611
olafenko <olopikolo666@gmail.com>
2024-01-05 20:23:50 +0100

    Create repository and service for app user

---
+++ src/main/java/com/RegistrationAndMailingApi/AppUser/Repo/AppUserRepository.java
@@ -0,0 +1,15 @@
+package com.RegistrationAndMailingApi.AppUser.Repo;
+
+import com.RegistrationAndMailingApi.AppUser.Model.AppUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface AppUserRepository extends JpaRepository<AppUser,Long> {
+
+    Optional<AppUser> findByEmail (String email);
+
+
+}
